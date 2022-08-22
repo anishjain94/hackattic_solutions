32cb69da2541a7d15c47f08eef91673b655b91aa
anish-pice <anish@pice.one>
2022-08-22 23:31:21 +0530

    :art: added generic for api calls

--- pkg/brute_force_zip/brute_force_zip.go
+++ pkg/brute_force_zip/brute_force_zip.go
@@ -2,9 +2,9 @@ package bruteforcezip
 
 import (
 	"archive/zip"
-	"encoding/json"
 	"hackattic_solutions/pkg/common"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -14,20 +14,28 @@ type zipDto struct {
 	ZipUrl string `json:"zip_url"`
 }
 
+func listFile(file *zip.File) error {
+
+	fileRead, err := file.Open()
+	common.HandleError(err)
+
+	defer fileRead.Close()
+
+	str, err := ioutil.ReadAll(fileRead)
+	common.HandleError(err)
+
+	print(string(str))
+
+	return nil
+}
+
 func BruteForceZip() {
 
-	resp, err := http.Get("https://hackattic.com/challenges/brute_force_zip/problem?access_token=8e80fec0cbe25049")
-	var zipDto zipDto
+	problemUrl := "https://hackattic.com/challenges/brute_force_zip/problem?access_token=8e80fec0cbe25049"
 
-	if err == nil {
-		defer resp.Body.Close()
+	zipObj := common.GetResponse[zipDto](problemUrl)
 
-		if err := json.NewDecoder(resp.Body).Decode(&zipDto); err != nil {
-			log.Fatal("ooopsss! an error occurred, please try again")
-		}
-	}
-
-	respZip, err := http.Get(zipDto.ZipUrl)
+	respZip, err := http.Get(zipObj.ZipUrl)
 	common.HandleError(err)
 
 	defer respZip.Body.Close()
@@ -38,11 +46,15 @@ func BruteForceZip() {
 	_, err = io.Copy(zipFile, respZip.Body)
 	common.HandleError(err)
 
-	zipReadCloser, err := zip.OpenReader("../pkg/brute_force_zip/hackattic.zip")
+	read, err := zip.OpenReader("../pkg/brute_force_zip/hackattic.zip")
 	common.HandleError(err)
 
-	for _, file := range zipReadCloser.File {
-		print(file)
+	for _, file := range read.File {
+		if file.Name == "secret.txt" {
+			if err := listFile(file); err != nil {
+				log.Fatal("unable to open file")
+			}
+		}
 	}
 
 }
--- pkg/common/helpers.go
+++ pkg/common/helpers.go
@@ -1,7 +1,27 @@
 package common
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 func HandleError(err error) {
 	if err != nil {
 		print(err.Error())
 	}
+}
+
+func GetResponse[dto any](url string) *dto {
+
+	resp, err := http.Get(url)
+	HandleError(err)
+
+	defer resp.Body.Close()
+
+	var respDto dto
+
+	err = json.NewDecoder(resp.Body).Decode(&respDto)
+	HandleError(err)
+
+	return &respDto
 }
